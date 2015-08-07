b8598edefcafba070f8b5850b477d6d30f70d87f
christianstanfield <christian.stanfield@gmail.com>
2015-08-06 21:14:03 -0500

    refactored js logic and added up until game start

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+
   var playerName;
 
   hideObjects();
@@ -6,35 +7,69 @@ $(function () {
 
   $('#player-name').on('click', function () {
     playerName = $('input[name="name"]').val();
+    playerName = playerName.charAt(0).toUpperCase() + playerName.slice(1);
     $('.ask-name').fadeOut();
 
-    playerReady(playerName);
+    playerReady();
   });
-});
 
-var hideObjects = function () {
-  $('#welcome').children().hide();
-  $('#goodbye').hide();
-};
 
-var showWelcome = function () {
-  $('#welcome h1').fadeIn(1500);
-  $('#banner').delay(750).fadeIn(1500);
-  $('.ask-name').delay(1500).fadeIn(1500);
-};
+  function hideObjects() {
+    $('#welcome').children().hide();
+    $('#goodbye').hide();
+    $('#rumble').hide();
+  }
 
-var playerReady = function (playerName) {
-  var $playerWelcome = $('.ready-rumble h2:first-child');
-  var welcome = $playerWelcome.text().concat(playerName);
-  $playerWelcome.text(welcome);
-  $('.ready-rumble').delay(750).fadeIn(1000);
+  function showWelcome() {
+    $('#welcome h1').fadeIn(1500);
+    $('#banner').delay(750).fadeIn(1500);
+    $('.ask-name').delay(1500).fadeIn(1500);
+  }
 
-  $('input[type="checkbox"]').on('click', function () {
-    if ( $(this).val() === 'no') {
-      $('#goodbye').delay(500).fadeIn();
-    } else {
+  function playerReady() {
+    var $playerWelcome = $('.ready-rumble h2:first-child');
+    var welcome = $playerWelcome.text().concat(playerName);
+    $playerWelcome.text(welcome);
+    $('.ready-rumble').delay(750).fadeIn(1000);
 
+    $('input[type="checkbox"]').on('click', function () {
+      if ( $(this).val() === 'no') {
+        $('#goodbye').delay(500).fadeIn();
+      } else {
+        readyToRumble();
+      }
+      $('#welcome').fadeOut();
+    });
+  }
+
+  function readyToRumble() {
+    $('#player').text(playerName);
+    $('#rumble').delay(750).fadeIn(1500, function () {
+      rumble();
+    });
+  }
+
+  function rumble() {
+    while ( checkForWinner() ) {
+      moveCharacters();
+      attackCharacters();
     }
-    $('#welcome').fadeOut();
-  });
-};
+    showWinner();
+  }
+
+  function checkForWinner() {
+    // fill me in!
+  }
+
+  function moveCharacters() {
+    // and me!
+  }
+
+  function attackCharacters() {
+    // and me!
+  }
+
+  function showWinner() {
+    // and me three!
+  }
+});
